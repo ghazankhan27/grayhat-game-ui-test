0b0999c54c8dacaab4d47049abf2e3eb8cdc79eb
Ghazan <ghazankhan27@hotmail.com>
2024-06-02 17:45:20 +0500

    change url for prod

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   process.env.NODE_ENV === "production"
-    ? "http://localhost:3000"
+    ? "https://grayhat-backend.onrender.com/"
     : "http://localhost:3000";
 
 export const socket = io(URL);
